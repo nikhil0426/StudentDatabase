77d991d5f86804489bdc4b5e6626fddee3980c97
Nikhil <nikhil0426@gmail.com>
2021-03-14 05:24:02 -0400

    Added check in and attendance log for Student

--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,6 @@
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 public class Student extends Tz {
     /*
       items for student:
@@ -11,5 +14,23 @@ public class Student extends Tz {
       library loans
      */
 
+    public static ArrayList<String> attendance = new ArrayList<>();
 
+    public static void main(String[] args) {
+
+    }
+
+    public static void checkIn() {
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss");
+        getAttendance().add(simpleDateFormat.format(date));
+    }
+
+    public static ArrayList<String> getAttendance() {
+        return attendance;
+    }
+
+    public static void setAttendance(ArrayList<String> attendance) {
+        Student.attendance = attendance;
+    }
 }
